71d3ce11917363740a168c203cc84db21bfd8ab0
Angular Builds <circleci@angular.io>
2021-05-03 12:17:41 +0000

    c1512e427 build: update files to be eslint compliant

--- bin/architect.js
+++ bin/architect.js
@@ -173,7 +173,7 @@ async function main(args) {
 main(process.argv.slice(2)).then((code) => {
     process.exit(code);
 }, (err) => {
-    // tslint:disable-next-line: no-console
+    // eslint-disable-next-line no-console
     console.error('Error: ' + err.stack || err.message || err);
     process.exit(-1);
 });
