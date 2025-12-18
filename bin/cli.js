d97d08e1fc77dd90c142c29da6c577ac05d20b73
Angular Builds <dev-infra@angular.dev>
2025-12-18 12:50:58 +0000

    cf0512d refactor(@angular-devkit/architect): consolidate architect cli into core package

--- bin/cli.js
+++ bin/cli.js
@@ -6,4 +6,5 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE file at https://angular.dev/license
  */
-export {};
+
+import '@angular-devkit/architect/bin/architect.js';
